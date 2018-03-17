2f3d7184669f32b0873cf4a87dd6c6cd0dcffef0
furuhama <furuhama@users.noreply.github.com>
2018-03-17 22:02:18 +0900

    Add read_lines

--- ruvm.rb
+++ ruvm.rb
@@ -1,5 +1,30 @@
 # ruvm (= Ruby simple Virtual Machine)
 
 def parse(program)
-  program.split("\n").map(&:strip)
+  tokenize(program).map(&:convert)
+end
+
+def tokenize(string)
+  string.split("\n").map(&:strip)
+end
+
+def read_lines(str_array)
+  str_array.map(&:split).map { |line| line.map { |token| convert token } }
+end
+
+def convert(token)
+  type_casts = [
+    lambda { |arg| Integer arg },
+    lambda { |arg| Float(arg).to_i }, # convert Float argument to Integer
+    lambda { |arg| arg.to_sym },
+  ]
+
+  begin
+    type_casts.first.call(token)
+  rescue ArgumentError
+    # remove first element of type_casts
+    # and retry this method
+    type_casts.shift
+    retry
+  end
 end
--- spec/ruvm_spec.rb
+++ spec/ruvm_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 require './ruvm'
 
 describe 'ruvm' do
-  describe '#parse' do
-    subject { parse program }
+  describe '#tokenize' do
+    subject { tokenize program }
 
     context 'just string' do
       let(:program) { "this
@@ -22,6 +22,44 @@ describe 'ruvm' do
                       ruby simple stack machine" }
 
       it { is_expected.to eq ["this is a", "test program", "for", "ruby simple stack machine"] }
+    end
+  end
+
+  describe '#convert' do
+    subject { convert token }
+
+    context 'String' do
+      let (:token) { 'hogehoge' }
+
+      it { is_expected.to eq :hogehoge }
+    end
+
+    context 'Integer' do
+      let (:token) { '27362' }
+
+      it { is_expected.to eq 27362 }
+    end
+
+    context 'Float' do
+      let (:token) { '3.14' }
+
+      it { is_expected.to eq 3 }
+    end
+  end
+
+  describe 'read_lines' do
+    subject { read_lines lines }
+
+    context '1 word in each line' do
+      let(:lines) { ["hoge", "10", "5.5"] }
+
+      it { is_expected.to eq [[:hoge], [10], [5]] }
+    end
+
+    context 'more than 1 word in each line' do
+      let(:lines) { ["hoge 10 5.5", "piyo 15 3.3"] }
+
+      it { is_expected.to eq [[:hoge, 10, 5], [:piyo, 15, 3]] }
     end
   end
 end
