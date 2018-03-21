98ac054c0ff87e238a940b1a256285487c1bff59
furuhama <furuhama@users.noreply.github.com>
2018-03-21 17:30:33 +0900

    Edit names of specs

--- spec/ruvm_spec.rb
+++ spec/ruvm_spec.rb
@@ -2,73 +2,75 @@ require 'spec_helper'
 require './ruvm'
 
 describe 'ruvm' do
-  describe '.parse' do
-    subject { Parser.parse program }
+  describe 'Parser' do
+    describe '#parse' do
+      subject { Parser.parse program }
 
-    let(:program) { "this is a 723
-                    test program 100
-                    5.51 versions" }
-    it { is_expected.to eq [[:this, :is, :a, 723], [:test, :program, 100], [5, :versions]] }
-  end
-
-  describe '.tokenize' do
-    subject { Parser.tokenize program }
-
-    context 'just string' do
-      let(:program) { "this
-                      is
-                      a
-                      test
-                      program" }
-
-      it { is_expected.to eq ["this", "is", "a", "test", "program"] }
+      let(:program) { "this is a 723
+                      test program 100
+                      5.51 versions" }
+      it { is_expected.to eq [[:this, :is, :a, 723], [:test, :program, 100], [5, :versions]] }
     end
 
-    context 'more than one word in each line' do
-      let(:program) { "this is a
-                      test program
-                      for
-                      ruby simple stack machine" }
+    describe '#tokenize' do
+      subject { Parser.tokenize program }
 
-      it { is_expected.to eq ["this is a", "test program", "for", "ruby simple stack machine"] }
-    end
-  end
+      context 'just string' do
+        let(:program) { "this
+                        is
+                        a
+                        test
+                        program" }
 
-  describe '.convert' do
-    subject { Parser.convert token }
+        it { is_expected.to eq ["this", "is", "a", "test", "program"] }
+      end
 
-    context 'String' do
-      let (:token) { 'hogehoge' }
+      context 'more than one word in each line' do
+        let(:program) { "this is a
+                        test program
+                        for
+                        ruby simple stack machine" }
 
-      it { is_expected.to eq :hogehoge }
-    end
-
-    context 'Integer' do
-      let (:token) { '27362' }
-
-      it { is_expected.to eq 27362 }
+        it { is_expected.to eq ["this is a", "test program", "for", "ruby simple stack machine"] }
+      end
     end
 
-    context 'Float' do
-      let (:token) { '3.14' }
+    describe '#convert' do
+      subject { Parser.convert token }
 
-      it { is_expected.to eq 3 }
+      context 'String' do
+        let (:token) { 'hogehoge' }
+
+        it { is_expected.to eq :hogehoge }
+      end
+
+      context 'Integer' do
+        let (:token) { '27362' }
+
+        it { is_expected.to eq 27362 }
+      end
+
+      context 'Float' do
+        let (:token) { '3.14' }
+
+        it { is_expected.to eq 3 }
+      end
     end
-  end
 
-  describe '.read_lines' do
-    subject { Parser.read_lines lines }
+    describe '#read_lines' do
+      subject { Parser.read_lines lines }
 
-    context '1 word in each line' do
-      let(:lines) { ["hoge", "10", "5.5"] }
+      context '1 word in each line' do
+        let(:lines) { ["hoge", "10", "5.5"] }
 
-      it { is_expected.to eq [[:hoge], [10], [5]] }
-    end
+        it { is_expected.to eq [[:hoge], [10], [5]] }
+      end
 
-    context 'more than 1 word in each line' do
-      let(:lines) { ["hoge 10 5.5", "piyo 15 3.3"] }
+      context 'more than 1 word in each line' do
+        let(:lines) { ["hoge 10 5.5", "piyo 15 3.3"] }
 
-      it { is_expected.to eq [[:hoge, 10, 5], [:piyo, 15, 3]] }
+        it { is_expected.to eq [[:hoge, 10, 5], [:piyo, 15, 3]] }
+      end
     end
   end
 end
